feb299c5319c34e20f08c540ceca1e1870bd1145
Wolodja <wolodjawaskovych@i.ua>
2020-12-10 14:36:40 +0100

    Day 10 refactoring

--- src/main/java/day10/Task2.java
+++ src/main/java/day10/Task2.java
@@ -13,41 +13,31 @@ public class Task2 {
         List<String> allAdapters = inputReader.readInputLines("day10.txt");
         List<Integer> adapters = allAdapters.stream().map(Integer::parseInt).collect(Collectors.toList());
         adapters.add(0);
-        adapters.add(Collections.max(adapters)+3);
+        adapters.add(Collections.max(adapters) + 3);
         Collections.sort(adapters);
-        devideAdapters(adapters);
-    }
-
-
-    private void devideAdapters(List<Integer> adapters) {
         Long result = 1L;
-        int startIndex= 0;
+        int startIndex = 0;
         int currIndex = 0;
         int nextIndex = currIndex + 1;
-        while (startIndex != adapters.size()-1) {
+        while (startIndex != adapters.size() - 1) {
             while (adapters.get(nextIndex) == adapters.get(currIndex) + 1) {
                 nextIndex++;
                 currIndex++;
             }
             int diff = nextIndex - startIndex;
-            if(diff == 3){
-                result*=2;
-            } else if(diff == 4){
-                result*=4;
-            } else if(diff == 5){
-                result*=7;
+            if (diff == 3) {
+                result *= 2;
+            } else if (diff == 4) {
+                result *= 4;
+            } else if (diff == 5) {
+                result *= 7;
             }
             currIndex++;
             nextIndex++;
             startIndex = currIndex;
-
-
         }
         System.out.println(result);
-
-
     }
-
 
     public static void main(String[] args) {
         Task2 task2 = new Task2();
