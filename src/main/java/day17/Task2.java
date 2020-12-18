75d21570595467897a8bd317f49def4b61db3080
Wolodja <wolodjawaskovych@i.ua>
2020-12-18 15:30:15 +0100

    Day 17, Task 2

--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -9,35 +9,27 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Task1 {
+public class Task2 {
 
-    public static void main(String[] args) {
-        Task1 task1 = new Task1();
-        task1.part1();
-    }
-
-
-
-    public void part1() {
-        Set<Pos> pos = new HashSet<>();
+    public void part2() {
+        Set<Pos4> pos = new HashSet<>();
         InputReader inputReader = new InputReader();
         List<String> strings = inputReader.readInputLines("day17.txt");
         AtomicInteger currentIndex = new AtomicInteger();
-        Set<Pos> finalPos = pos;
+        Set<Pos4> finalPos = pos;
         strings.forEach(line -> {
             char[] chars = line.toCharArray();
             for(int i=0; i<chars.length; i++){
                 if(chars[i] == '#'){
-                    finalPos.add(new Pos(currentIndex.get(), i, 0));
+                    finalPos.add(new Pos4(currentIndex.get(), i, 0, 0));
                 }
             }
             currentIndex.getAndIncrement();
         });
-
         for(int i = 0; i<6; i++){
-            Set<Pos> newPos = new HashSet<>();
-            Set<Pos> checkedPos = new HashSet<>();
-            for(Pos p : pos) {
+            Set<Pos4> newPos = new HashSet<>();
+            Set<Pos4> checkedPos = new HashSet<>();
+            for(Pos4 p : pos) {
                 addNeighbors(pos, newPos, checkedPos, p, true);
             }
             pos = newPos;
@@ -45,18 +37,20 @@ public class Task1 {
         System.out.println(pos.size());
     }
 
-    public void addNeighbors(Set<Pos> pos, Set<Pos> newPos, Set<Pos> checkedPos, Pos p, boolean active){
+    public void addNeighbors(Set<Pos4> pos, Set<Pos4> newPos, Set<Pos4> checkedPos, Pos4 p, boolean active){
         if(!checkedPos.contains(p)) {
             long neighbours = active ? -1 : 0;
             checkedPos.add(p);
             for (int a = -1; a <= 1; a++) {
                 for (int b = -1; b <= 1; b++) {
                     for (int c = -1; c <= 1; c++) {
-                        Pos x = new Pos(p.x + a, p.y + b, p.z + c);
-                        if (pos.contains(x)) {
-                            neighbours++;
-                        } else if(active) {
-                            addNeighbors(pos, newPos, checkedPos, x, false);
+                        for (int d = -1; d <= 1; d++) {
+                            Pos4 x = new Pos4(p.x + a, p.y + b, p.z + c, p.w + d);
+                            if (pos.contains(x)) {
+                                neighbours++;
+                            } else if (active) {
+                                addNeighbors(pos, newPos, checkedPos, x, false);
+                            }
                         }
                     }
                 }
@@ -70,9 +64,15 @@ public class Task1 {
 
     @Data
     @Value
-    public static class Pos {
+    public static class Pos4 {
         long x;
         long y;
         long z;
+        long w;
+    }
+
+    public static void main(String[] args) {
+        Task2 task2 = new Task2();
+        task2.part2();
     }
 }
